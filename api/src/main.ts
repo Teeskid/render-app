e5cf7e0b1fc6ea910a9f40bd36987ea0f382fc64
Teeskid <aaedyteeskid@gmail.com>
2023-11-18 16:06:52 +0000

    quick-push

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,21 +11,22 @@ app.all("/", (req, res) => {
     res.sendStatus(200)
 })
 
-app.all("/webhook", async (req: Request<{}, string, UssdRequest>, res: Response<string>) => {
+app.all("/callback", async (req: Request<{}, string, UssdRequest>, res: Response<string>) => {
     // send message to the logger on node
     console.log("CODE RECEIVED")
     console.info(req.body)
 
+    // Read the variables sent via POST from our API
+    let {
+        sessionId,
+        serviceCode,
+        phoneNumber,
+        text,
+    } = req.body
+
     // We are returning a response anyway
     let response: string = "";
     try {
-        // Read the variables sent via POST from our API
-        let {
-            sessionId,
-            serviceCode,
-            phoneNumber,
-            text,
-        } = req.body;
 
         // sanitize input
         sessionId = String(sessionId).trim()
@@ -33,41 +34,44 @@ app.all("/webhook", async (req: Request<{}, string, UssdRequest>, res: Response<
         phoneNumber = String(phoneNumber).trim()
         text = String(text).trim()
 
+        // deal with the real handling now
         if (text.indexOf("*") !== -1) {
             // long multi session ussd
             response = "END USSD SERVICE IS UNDER MAINTAINANCE, PLEASE TRY LATER"
         } else {
-            getTopic().publishMessage({
-                json: {
-                    sessionId,
-                    serviceCode,
-                    phoneNumber,
-                    text
-                }
-            })
             response = "You line has been credited with 1GB SME Data successfully. Dial *323*4# to check balance."
         }
     } catch (error: Error | unknown) {
         console.error('ERRORROR', error)
         response = `END ${(error as Error).message}`
+    } finally {
+        if (!response || response.length < 5)
+            response = "USSD Working Perfectly In Development"
+
+        // Send the response back to the API
+        res.set("Content-Type: text/plain")
+        res.send(response)
+        // begin the main job now
+        const id = await getTopic().publishMessage({
+            json: {
+                sessionId,
+                serviceCode,
+                phoneNumber,
+                text
+            }
+        })
     }
-    if (!response || response.length < 5)
-        response = "USSD Working Perfectly In Development"
-    // Send the response back to the API
-    res.set("Content-Type: text/plain");
-    res.send(response);
 })
 
-app.all("/callback", async (req: Request<{}, string, UssdCallback>, res) => {
+app.all("/events", async (req: Request<{}, string, UssdCallback>, res) => {
+    // Read the variables sent via POST from our API
+    let {
+        sessionId,
+        serviceCode,
+        phoneNumber,
+        input,
+    } = req.body;
     try {
-        // Read the variables sent via POST from our API
-        let {
-            sessionId,
-            serviceCode,
-            phoneNumber,
-            input,
-        } = req.body;
-
         // sanitize the inputs
         sessionId = String(sessionId).trim()
         serviceCode = String(serviceCode).trim()
@@ -79,11 +83,12 @@ app.all("/callback", async (req: Request<{}, string, UssdCallback>, res) => {
         } else {
             // do something else
         }
-
-        // Send the response back to the API
-        res.set("Content-Type: text/plain");
-        res.sendStatus(200)
     } catch (error: Error | unknown) {
+    } finally {
+        // Send the response back to the API
+        res.status(200)
+        res.set("Content-Type: text/plain")
+        res.send("END Done")
     }
 })
 
