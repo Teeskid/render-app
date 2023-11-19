d32a718bc8f25217c80935eee59ca659e7164514
Teeskid <aaedyteeskid@gmail.com>
2023-11-19 06:10:51 +0000

    quick-push

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,49 +1,60 @@
 import "dotenv/config"
 
-import { credential } from "firebase-admin"
 import { initializeApp } from "firebase-admin/app"
-import path from "path"
+import { getFirestore } from "firebase-admin/firestore"
 import { pid } from "process"
 
-const CREDENTIALS = credential.cert(path.join(__dirname, "../techify-ng-073c85f2027d.json"))
 const SERVER_PORT = process.env.PORT || 8888
 
-initializeApp({
-	credential: CREDENTIALS,
-	projectId: "techify-ng",
-	databaseURL: "https://techify-ng.firebaseio.com",
-	storageBucket: "techify-ng.appspot.com",
-	// serviceAccountId: "rnd-service",
-	// apiKey: "AIzaSyDwMERWGhJ2SI51pVqmBDMADkgUM2vdWlA",
-	// authDomain: "techify-ng.firebaseapp.com",
-	// messagingSenderId: "25233989097",
-	// appId: "1:25233989097:web:73ffc11986024f3f957f78",
-	// measurementId: "G-RPE7K4ZL15",
-})
+// initializeApp({
+// 	credential: CREDENTIALS,
+// 	projectId: "techify-ng",
+// 	databaseURL: "https://techify-ng.firebaseio.com",
+// 	storageBucket: "techify-ng.appspot.com",
+// 	// serviceAccountId: "rnd-service",
+// 	// apiKey: "AIzaSyDwMERWGhJ2SI51pVqmBDMADkgUM2vdWlA",
+// 	// authDomain: "techify-ng.firebaseapp.com",
+// 	// messagingSenderId: "25233989097",
+// 	// appId: "1:25233989097:web:73ffc11986024f3f957f78",
+// 	// measurementId: "G-RPE7K4ZL15",
+// })
 
-import "./utils/client"
+initializeApp()
+
+// import "./utils/client"
 
 import app from "./main"
-import { loadTopic } from "./utils/client"
+import { PubSub } from "@google-cloud/pubsub"
 
-app.on("upgrade", () => {
-	console.log("HELLO-WEBSOCKET")
+const pubsub = new PubSub({ projectId: "techify-ng" })
+
+pubsub.topic("sim-service").get().then(() => {
+	console.log("TRUEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 })
 
-process.on("SIGINT", () => {
-	process.kill(pid)
-})
 
-console.log("LOADING CLOUD PUBSUB TOPIC...")
-loadTopic().then(() => {
-	console.log("CLOUD PUBSUB TOPIC LOADED")
-	console.log("STARTING NODE SERVER...")
-	app.listen(SERVER_PORT, () => {
-		console.log(`SERVING API ON PORT ${SERVER_PORT}`)
-	})
-}).catch((error) => {
-	console.error(error)
-	process.emit("SIGINT")
-})
+// app.on("upgrade", () => {
+// 	console.log("HELLO-WEBSOCKET")
+// })
+
+// process.on("SIGINT", () => {
+// 	process.kill(pid)
+// })
+
+// getFirestore().collection("posts").get().then((d) => {
+// 	console.log(d)
+// })
+
+// console.log("LOADING CLOUD PUBSUB TOPIC...")
+// loadTopic().then(() => {
+// 	console.log("CLOUD PUBSUB TOPIC LOADED")
+// 	console.log("STARTING NODE SERVER...")
+// 	app.listen(SERVER_PORT, () => {
+// 		console.log(`SERVING API ON PORT ${SERVER_PORT}`)
+// 	})
+// }).catch((error) => {
+// 	console.error(error)
+// 	process.emit("SIGINT")
+// })
 
 export default {}
--- api/src/utils/client.ts
+++ api/src/utils/client.ts
@@ -1,24 +1,17 @@
 import { PubSub, Topic } from "@google-cloud/pubsub"
-import { getApp } from "firebase-admin/app"
 
 let mainTopic: Topic | null = null
 
 export const loadTopic = async () => {
     const pubSubClient = new PubSub({
-        projectId: getApp().options.projectId
+        projectId: "techify-ng"
     })
-    await pubSubClient.topic("projects/techify-ng/topics/rnd-service").get().then(([topic]) => {
-        mainTopic = topic
-    }).catch((error) => {
-        mainTopic = null
-        throw error
-    })
+    const [topic] = await pubSubClient.topic("rnd-service").get()
+    return topic
 }
 
 export const getTopic = (): Topic => {
-    if (mainTopic === null) {
-        loadTopic()
-    } else {
+    if (mainTopic !== null) {
         return mainTopic
     }
     throw new Error("topic not initialized yet")
